d7bc5149c3bb0d75cca0a9979b91bc09aa74aa17
Yehor Kravchenko <brazuca.egor@gmail.com>
2023-12-17 17:55:04 +0200

    Initial commit

---
+++ src/test/java/com/kravchenko/photoapp/discovery/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kravchenko.photoapp.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
